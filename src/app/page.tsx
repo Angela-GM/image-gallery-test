87ad34cd251d60db8d2127c2d981a0dbbe76d78e
Angela <agarcia@grupomayo.com>
2026-03-12 20:56:37 +0100

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-50">
+      <h1 className="text-3xl font-bold text-center mb-8">
+        Prueba Técnica EY - Ángela
+      </h1>
+      {/* Aquí irá nuestro PhotoGrid luego */}
+    </main>
+  );
+}
