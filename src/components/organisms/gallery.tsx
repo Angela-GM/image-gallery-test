b474805a54548e365fc677df1e6a2a7a8cf063b6
Angela <agarcia@grupomayo.com>
2026-03-12 22:47:24 +0100

    feat: implement infinite scrolling with IntersectionObserver

--- src/components/organisms/gallery.tsx
+++ src/components/organisms/gallery.tsx
@@ -1,18 +1,50 @@
-'use client';
+"use client";
 
 import { usePhotos } from "@/hooks/usePhotos";
 import { Loader } from "../atoms/loader";
 import { ErrorMessage } from "../atoms/error-message";
 import { PhotoGrid } from "./photo-grid";
+import { useEffect, useRef } from "react";
 
 export const Gallery = () => {
-    const {photos, isLoading, error} = usePhotos();
+  const { photos, isLoading, error, removePhoto, loadMore, hasMore } =
+    usePhotos();
 
-    if (isLoading) return <Loader text="Cargando fotos..." />
-    if (error) return <ErrorMessage message={error} />
-    if (photos.length === 0) return <ErrorMessage message="No se encontraron fotos" />
+  const observerTarget = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore && !isLoading) {
+          loadMore();
+        }
+      },
+      { threshold: 0.5 },
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [loadMore, hasMore, isLoading]);
+
+  if (error && photos.length === 0) return <ErrorMessage message={error} />;
 
   return (
-    <PhotoGrid photos={photos} />
-  )
-}
+    <div>
+      {photos.length > 0 && <PhotoGrid photos={photos} />}
+
+      <div ref={observerTarget}></div>
+
+      {isLoading && <Loader text="Cargando fotos..." />}
+
+      {!hasMore && photos.length > 0 && (
+        <ErrorMessage message="No se encontraron más fotos" />
+      )}
+    </div>
+  );
+};
--- src/hooks/usePhotos.ts
+++ src/hooks/usePhotos.ts
@@ -1,52 +1,69 @@
 import { Photo } from "@/types/photo";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const usePhotos = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchPhotos = async () => {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/photos?_limit=20",
-        );
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
-        if (!response.ok) {
-          throw new Error(`Error HTTP: ${response.status}`);
-        }
+  const fetchPhotos = async (currentPage: number) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=50`,
+      );
 
-        const data: Photo[] = await response.json();
-        console.log(data);
+      if (!response.ok) {
+        throw new Error(`Error HTTP: ${response.status}`);
+      }
 
+      const data: Photo[] = await response.json();
+
+      if (data.length === 0) {
+        setHasMore(false);
+        return;
+      } else {
         const fixedData = data.map((photo) => ({
           ...photo,
           url:
             photo.url.replace("via.placeholder.com", "placehold.co") +
             "/FFF.png",
           thumbnailUrl:
-            photo.thumbnailUrl.replace("via.placeholder.com", "placehold.co") +
-            "/FFF.png",
+            photo.thumbnailUrl.replace(
+              "via.placeholder.com",
+              "placehold.co",
+            ) + "/FFF.png",
         }));
-        setPhotos(fixedData);
-      } catch (error) {
-        setError(
-          error instanceof Error
-            ? error.message
-            : "Error desconocido al cargar las fotos",
-        );
-      } finally {
-        setIsLoading(false);
+        setPhotos((currentPhotos) => [...currentPhotos, ...fixedData]);
       }
-    };
-
-    fetchPhotos();
-  }, []);
-
-  return {
-    photos,
-    isLoading,
-    error,
+    } catch (error) {
+      setError(
+        error instanceof Error
+          ? error.message
+          : "Error desconocido al cargar las fotos",
+      );
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  useEffect(() => {
+    if (hasMore) {
+      fetchPhotos(page);
+    }
+  }, [page]);
+
+  const loadMore = useCallback(() => {
+    if (!isLoading && hasMore) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  }, [isLoading, hasMore]);
+
+  const removePhoto = (id: number) => {
+    setPhotos((currentPhotos) => currentPhotos.filter((photo) => photo.id !== id));
+  };
+
+  return { photos, isLoading, error, removePhoto, loadMore, hasMore };
 };
