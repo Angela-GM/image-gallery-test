1771f978c3d5abc0670012e2a53cfafea1238c67
Angela <agarcia@grupomayo.com>
2026-03-15 00:28:53 +0100

    add fallback .env

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_ENDPOINT_BASE as string;
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_ENDPOINT_BASE || "https://jsonplaceholder.typicode.com";
